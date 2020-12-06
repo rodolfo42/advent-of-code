3147549ae2d956c5dfb3251979494ccb0174078d
Rodolfo Ferreira <rodolfo@rodolfoferreira.com.br>
2020-12-05 21:34:27 -0300

    day 3 part 2

--- src/advent/day_3.clj
+++ src/advent/day_3.clj
@@ -11,30 +11,41 @@
 
 (def tree \#)
 
-(defn tap [x]
-  (println x)
-  x)
-
 (defn is-tree-at [tree-map pos]
   (->> tree-map first (drop pos) first (= tree)))
 
-(defn count-trees [tree-map]
-  (loop [map'       (drop 1 tree-map)
-         slope-x    3
-         tree-count 0]
+(defn count-trees [tree-map slope-right slope-down]
+  (loop [map'         (drop slope-down tree-map)
+         tree-count   0
+         slope-right' slope-right]
     (if (seq map')
-      (recur (next map')
-             (+ slope-x 3)
-             (if (is-tree-at map' slope-x)
+      (recur (drop slope-down map')
+             (if (is-tree-at map' slope-right')
                (inc tree-count)
-               tree-count))
+               tree-count)
+             (+ slope-right' slope-right))
       tree-count)))
+
+default-data-readers
 
 (defn part-1 []
   (let [tree-map (read-input "day-3.input")]
-    (count-trees tree-map)))
+    (count-trees tree-map 3 1)))
+
+(defn part-2 []
+  (let [tree-map    (read-input "day-3.input")
+        tree-counts (for [[right down] [[1 1]
+                                        [3 1]
+                                        [5 1]
+                                        [7 1]
+                                        [1 2]]]
+                      (count-trees tree-map right down))]
+    (apply * tree-counts)))
 
 (comment
   (part-1)
-  ;272
+  ; 272
+
+  (part-2)
+  ; 3898725600
   )
