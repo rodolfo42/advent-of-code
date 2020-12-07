4c2a4fbede00cbb5069c4a4aa3e514e271b9cb9f
Rodolfo Ferreira <rodolfo@rodolfoferreira.com.br>
2020-12-06 21:39:29 -0300

    add part 2

--- test/advent/day_6_test.clj
+++ test/advent/day_6_test.clj
@@ -1,6 +1,9 @@
 (ns advent.day-6-test
   (:require [clojure.test :refer :all]
-            [advent.day-6 :refer [part-1]]))
+            [advent.day-6 :refer [part-1 part-2]]))
 
 (deftest part-1-test
   (is (= (part-1) 6297)))
+
+(deftest part-2-test
+  (is (= (part-2) 3158)))
