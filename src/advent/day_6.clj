a8a2edaaced12b1fe57e600ab54679d18142cdd4
Rodolfo Ferreira <rodolfo@rodolfoferreira.com.br>
2020-12-06 21:38:03 -0300

    day 6 part 2

--- src/advent/day_6.clj
+++ src/advent/day_6.clj
@@ -7,16 +7,32 @@
        line-seq
        (split-in-batches-by str/blank?)))
 
-(defn find-yes-answers [people-answers]
+(defn find-any-yes-answers [people-answers]
   (set (apply concat people-answers)))
+
+(defn all-yes? [question answers]
+  (every? #(some #{question} %) answers))
+
+(defn find-all-yes-answers [group-answers]
+  (let [answers   (map seq group-answers)
+        questions (map char (range (int \a) (inc (int \z))))]
+    (filter #(all-yes? % answers) questions)))
 
 (defn part-1 []
   (->> (read-input #advent/input "day-6")
-       (map (comp count find-yes-answers))
+       (map (comp count find-any-yes-answers))
+       (apply +)))
+
+(defn part-2 []
+  (->> (read-input #advent/input "day-6")
+       (map (comp count find-all-yes-answers))
        (apply +)))
 
 (comment
 
   (part-1)
   ; 6297
+
+  (part-2)
+  ; 3158
   )
