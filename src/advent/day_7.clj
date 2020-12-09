d8ad21b43db3a049aa8034a45c056a5a2ee33103
Rodolfo Ferreira <rodolfo@rodolfoferreira.com.br>
2020-12-08 22:31:47 -0300

    day 7 part 2

--- src/advent/day_7.clj
+++ src/advent/day_7.clj
@@ -9,7 +9,7 @@
     nil
     (let [matches (re-matches #"([0-9]+) ([\w]+ [\w]+) bag(s*)" s)
           [qty color] (rest matches)]
-      #{color})))
+      #{[color (Integer/parseInt qty)]})))
 
 (defn process-rules [s]
   (->> (str/split s #",")
@@ -21,34 +21,50 @@
   (let [[color rule] (rest (re-matches #"([a-z]+ [a-z]+) bags contain ([0-9a-z ,]+)." s))]
     (hash-map color (process-rules rule))))
 
-(defn expand-colors [rules color]
-  (get rules color))
-
 (defn read-input [input]
   (->> input
-       line-seq
        (map parse-line)
        (apply merge)))
 
 (defn flatten-colors [m]
   (->> m
-       (map #(tree-seq m m (first %)))
-       (map #(hash-map (first %) (set (rest %))))
-       (apply merge)))
+       (map (fn [[k v]] [k (set (map first v))]))
+       (into {})))
 
 (defn find-possible-outermost-bags
   [rules search-color]
-  (->> rules
-       flatten-colors
-       (filter #(some #{search-color} (val %)))
-       (map first)))
+  (let [flattened (flatten-colors rules)]
+    (->> flattened
+         keys
+         (map #(->> % (tree-seq flattened flattened) rest))
+         (filter #(some #{search-color} %)))))
+
+(defn find-bag-contents
+  [rules color]
+  (tree-seq #(-> % first rules)
+            (fn [[c q]]
+              (->> (get rules c)
+                   (map (fn [[k v]] [k (* v q)]))))
+            [color 1]))
 
 (defn part-1 []
   (let [input (read-input #advent/input "day-7")]
     (count (find-possible-outermost-bags input "shiny gold"))))
 
+(defn part-2 []
+  (let [input (read-input #advent/input "day-7")]
+    (->> "shiny gold"
+         (find-bag-contents input)
+         (map second)
+         (apply +)
+         dec)))
+
 (comment
 
   (part-1)
   ; 115
+
+  (part-2)
+  ; 1250
   )
+
