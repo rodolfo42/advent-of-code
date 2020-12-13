a56892c53329069a2c969e4ef2d960c5fec6c5be
Rodolfo Ferreira <rodolfo@rodolfoferreira.com.br>
2020-12-13 00:11:37 -0300

    better day 1

--- src/advent/day_1.clj
+++ src/advent/day_1.clj
@@ -1,36 +1,27 @@
 (ns advent.day-1)
 
 (defn read-numbers [input]
-  (->> input
-       (mapv #(Integer/parseInt %))))
+  (map #(Integer/parseInt %) input))
 
-(defn find-two-nums [numbers target]
-  (let [found (atom nil)]
-    (doseq [n numbers
-            :while (nil? @found)]
-      (let [search (- target n)]
-        (when (some #{search} numbers)
-          (reset! found [n search]))))
-    @found))
+(defn find-two-nums [target numbers]
+  (some #(some-> (some #{(- target %)} numbers)
+                 (cons [%]))
+        numbers))
 
-(defn find-three-nums [numbers target]
-  (let [found (atom nil)]
-    (doseq [n numbers
-            :while (nil? @found)]
-      (let [search (- target n)]
-        (when-let [two-nums (find-two-nums numbers search)]
-          (reset! found (conj two-nums n)))))
-    @found))
+(defn find-three-nums [target numbers]
+  (some #(some-> (find-two-nums (- target %) numbers)
+                 (conj %))
+        numbers))
 
 (defn part-1 []
-  (let [numbers (read-numbers #advent/input "day-1")]
-    (when-let [two-nums (find-two-nums numbers 2020)]
-      (apply * two-nums))))
+  (some->> (read-numbers #advent/input "day-1")
+           (find-two-nums 2020)
+           (apply *)))
 
 (defn part-2 []
-  (let [numbers (read-numbers #advent/input "day-1")]
-    (when-let [three-nums (find-three-nums numbers 2020)]
-      (apply * three-nums))))
+  (some->> (read-numbers #advent/input "day-1")
+           (find-three-nums 2020)
+           (apply *)))
 
 
 (comment
